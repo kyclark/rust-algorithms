2bd83d54a9522aa5dca088659af9af18bf5a6920
Ken Youens-Clark <kyclark@gmail.com>
2021-11-23 14:10:52 -0700

    added tests

--- max_tournament/src/main.rs
+++ max_tournament/src/main.rs
@@ -1,17 +1,20 @@
 use std::{fmt::Debug, mem};
 
 fn main() {
-    for vals in &[vec![], vec![1], vec![10, 1], vec![2, 1, 3, 4, 1]] {
+    for vals in vec![vec![], vec![1], vec![10, 1], vec![2, 1, 3, 4, 1]] {
         match max_tournament(&vals) {
             Some(i) => println!("{:?} = {}", &vals, &vals[i]),
             _ => println!("{:?} = NA", &vals),
         }
     }
+    let vals = vec!["foo", "bar", "baz"];
+    match max_tournament(&vals) {
+        Some(i) => println!("{:?} = {}", &vals, &vals[i]),
+        _ => println!("{:?} = NA", &vals),
+    }
 }
 
-fn max_tournament<T: Debug + PartialEq + std::cmp::PartialOrd>(
-    vals: &[T],
-) -> Option<usize> {
+fn max_tournament<T: Debug + PartialEq + Ord>(vals: &[T]) -> Option<usize> {
     let mut positions: Vec<_> = (0..vals.len()).collect();
     while positions.len() > 1 {
         let mut winners: Vec<usize> = vec![];
@@ -29,4 +32,12 @@ fn max_tournament<T: Debug + PartialEq + std::cmp::PartialOrd>(
         positions = mem::take(&mut winners);
     }
     positions.get(0).copied()
+}
+
+#[test]
+fn test() {
+    let empty: &[usize] = &[];
+    assert_eq!(max_tournament(empty), None);
+    assert_eq!(max_tournament(&[3, 1, 2]), Some(0));
+    assert_eq!(max_tournament(&["foo", "bar", "baz"]), Some(0));
 }
